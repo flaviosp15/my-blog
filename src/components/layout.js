202b81d6dd1097dc9f6819028664fce0ea1bf9e6
flaviosp15 <flaviosp15@outlook.com>
2025-06-18 08:59:02 -0300

    feat: query site metadata title and insert into header element

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import { Link } from 'gatsby';
-import { container, heading, navLinks, navLinkItem, navLinkText } from './layout.module.css';
+import { Link, useStaticQuery, graphql } from 'gatsby';
+import { container, heading, navLinks, navLinkItem, navLinkText, siteTitle } from './layout.module.css';
 
 function Layout({ pageTitle, children }) {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
   return (
     <div className={container}>
+      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
       <nav>
         <ul className={navLinks}>
           <li className={navLinkItem}>
