058ae841799016930e2cfbca21e8a8760a30f7b7
flaviosp15 <flaviosp15@outlook.com>
2025-06-25 08:36:44 -0300

    feat: pull data from MDX files to blog page elements

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,20 +6,29 @@ import { graphql } from 'gatsby';
 function Blog({ data }) {
   return (
     <Layout pageTitle="My Blog Posts">
-      <ul>
-        {data.allFile.nodes.map(node => (
-          <li key={node.id}>{node.name}</li>
-        ))}
-      </ul>
+      {data.allMdx.nodes.map(node => (
+        <article key={node.id}>
+          <h2>{node.frontmatter.title}</h2>
+          <p>Posted: {node.frontmatter.date}</p>
+          <p>{node.excerpt}</p>
+        </article>
+      ))}
     </Layout>
   );
 }
 
 export const query = graphql`
   query {
-    allFile(filter: { sourceInstanceName: { eq: "blog" } }) {
+    allMdx(sort: { frontmatter: { date: DESC } }) {
       nodes {
-        name
+        id
+        excerpt
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          author
+          slug
+          title
+        }
       }
     }
   }
