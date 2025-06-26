f30a71c9101a96e148f75e368cbfface2aecfb84
flaviosp15 <flaviosp15@outlook.com>
2025-06-26 07:44:01 -0300

    feat: add dynamic blog page that refers to a specific post

---
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Layout from '../../components/layout';
+import Seo from '../../components/seo';
+import { graphql } from 'gatsby';
+
+function BlogPost({ data, children }) {
+  console.log(children);
+
+  return (
+    <Layout pageTitle={data.mdx.frontmatter.title}>
+      <p>{data.mdx.frontmatter.date}</p>
+      {children}
+      <p>Written by: {data.mdx.frontmatter.author}</p>
+    </Layout>
+  );
+}
+
+export const query = graphql`
+  query ($id: String) {
+    mdx(id: { eq: $id }) {
+      id
+      frontmatter {
+        title
+        date(formatString: "MMMM D, YYYY")
+        author
+        slug
+      }
+    }
+  }
+`;
+
+export const Head = ({ data }) => <Seo title={data.mdx.frontmatter.title} />;
+
+export default BlogPost;
